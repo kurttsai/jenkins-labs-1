ea1edd209ade0ae2450e52bc85aef800f9d66583
kurttsai <kurt_free@hotmail.com>
2015-12-15 15:23:30 +0800

    add bug

--- Tests/Nop.Core.Tests/WebHelperTests.cs
+++ Tests/Nop.Core.Tests/WebHelperTests.cs
@@ -23,7 +23,7 @@ namespace Nop.Core.Tests
             _webHelper.ServerVariables("Key1").ShouldEqual("Value1");
             _webHelper.ServerVariables("Key2").ShouldEqual("Value2");
             _webHelper.ServerVariables("Key3").ShouldEqual("");
-            int a=int.Parse("aaa");
+            int a="aaa";
         }
 
         [Test]
